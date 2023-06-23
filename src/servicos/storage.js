81fe4b046485389185a58c42cb6eb91c466bda79
rmo02 <ramoon.maia23@gmail.com>
2023-06-23 16:47:40 -0300

    utilizando storage

---
+++ src/servicos/storage.js
@@ -0,0 +1,32 @@
+import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
+import { storage } from "../config/firebase";
+
+export async function salvarImagem(imagem, imagemNome){
+    if(!imagem) return;
+
+    const downloadImagem = await fetch(imagem)
+    const blobImagem = await downloadImagem.blob()
+    const imagemRef = ref(storage, `posts/${imagemNome}.png`)
+
+    try {
+        await uploadBytes(imagemRef, blobImagem);
+        const url = await getDownloadURL(imagemRef);
+        return url
+    } catch (error) {
+        console.log(error)
+        return null;
+    }
+
+}
+
+export async function deletarImagem(postId){
+    const refStorage = ref(storage, `posts/${postId}.png`);
+    try {
+        await deleteObject(refStorage);
+        return true;
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
+}
+
